3c06609f408d1367362b71559a10e19c91511be6
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-03 19:50:50 -0400

    add stdin

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
+const { stdin } = require('process');
 const { connect } = require('./client.js');
 console.log("Connecting ...");
 connect();
+
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on('data', (key) => {
+        if (key === '\u0003'){
+            process.exit();
+        }
+    
+    });
+    
+    return stdin;
+};
+
+
+setupInput();
