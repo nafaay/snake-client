447799aa9bd8569de340fc9834f47a4cf9cabe08
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-03 19:58:59 -0400

    add more refactor

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { stdin } = require('process');
 const { connect } = require('./client.js');
+const { setupInput } = require('./input.js');
+
 console.log("Connecting ...");
 connect();
-
-
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on('data', (key) => {
-        if (key === '\u0003'){
-            process.exit();
-        }
-    
-    });
-    
-    return stdin;
-};
-
-
 setupInput();
